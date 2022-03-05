08be9292ac3267e0ac926da81abe980cb73e9c18
GioBCondor <ybeltran@condorlabs.io>
2022-03-05 16:28:02 -0500

    Initial Commit

---
+++ DataCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataCapture
+//
+//  Created by Yermis Beltran on 5/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
